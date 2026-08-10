876f7c60a9a7fab91434a97f95f9eebfb114ae32
Mohammad Hosseini <mimhe1381@gmail.com>
2026-08-10 14:42:47 +0330

    Add initial feature modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,22 @@ import { LoggerModule } from 'nestjs-pino';
 import { randomUUID } from 'crypto';
 import { AppConfigModule } from './common/config/config.module';
 import { PrismaModule } from './common/database/prisma.module';
+import { RedisModule } from './common/redis/redis.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { WorkspaceModule } from './modules/workspace/workspace.module';
+import { BoardModule } from './modules/board/board.module';
+import { DocumentModule } from './modules/document/document.module';
+import { ActivityModule } from './modules/activity/activity.module';
+import { NotificationModule } from './modules/notification/notification.module';
+import { FileModule } from './modules/file/file.module';
 
 @Module({
   imports: [
+    // Global Core & Infrastructure Modules
     AppConfigModule,
     PrismaModule,
+    RedisModule,
+
     // Centralized Structured Logging (Pino)
     LoggerModule.forRootAsync({
       inject: [ConfigService],
@@ -54,6 +65,15 @@ import { PrismaModule } from './common/database/prisma.module';
         },
       }),
     }),
+
+    // Feature Modules
+    AuthModule,
+    WorkspaceModule,
+    BoardModule,
+    DocumentModule,
+    ActivityModule,
+    NotificationModule,
+    FileModule,
   ],
 })
 export class AppModule {}
