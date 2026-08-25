8b843004360c065f8cce1f2fd5a8ea25e9a5d282
Mohammad Hosseini <mimhe1381@gmail.com>
2026-08-25 16:22:59 +0330

    Move `Activity` repository and listener out of `Board` module

--- src/modules/activity/activity.module.ts
+++ src/modules/activity/activity.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ActivityRepository } from './repositories/activity.repository';
+import { ActivityListener } from './listeners/activity.listener';
 
+/**
+ * Append-only activity audit log. Consumes domain events emitted by other
+ * modules via EventEmitter2; imports none of them (boundary rule).
+ */
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
-  exports: [],
+  providers: [ActivityRepository, ActivityListener],
+  exports: [ActivityRepository],
 })
 export class ActivityModule {}
