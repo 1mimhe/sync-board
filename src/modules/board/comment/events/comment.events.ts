854f4b78f8eb1de8cc4d30c03e7d1751499f4b02
Mohammad Hosseini <mimhe1381@gmail.com>
2026-08-29 02:27:00 +0330

    Implement comment update and delete event payloads

--- src/modules/board/comment/events/comment.events.ts
+++ src/modules/board/comment/events/comment.events.ts
@@ -8,3 +8,22 @@ export class CommentCreatedEvent {
     public readonly authorId: string,
   ) {}
 }
+
+/** Event emitted after a comment is edited. */
+export class CommentUpdatedEvent {
+  constructor(
+    public readonly comment: CardComment,
+    public readonly boardId: string,
+    public readonly updatedBy: string,
+  ) {}
+}
+
+/** Event emitted after a comment is soft-deleted. */
+export class CommentDeletedEvent {
+  constructor(
+    public readonly commentId: string,
+    public readonly cardId: string,
+    public readonly boardId: string,
+    public readonly deletedBy: string,
+  ) {}
+}
