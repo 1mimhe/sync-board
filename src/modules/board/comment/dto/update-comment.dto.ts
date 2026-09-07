6b915a88a6b4b8aa2a40cbbdc813401274ae7f02
Mohammad Hosseini <mimhe1381@gmail.com>
2026-09-07 11:38:46 +0330

    Implement `CommentService` threading and parent reply hierarchy

--- src/modules/board/comment/dto/update-comment.dto.ts
+++ src/modules/board/comment/dto/update-comment.dto.ts
@@ -1,7 +1,9 @@
-import { PartialType } from '@nestjs/swagger';
+import { OmitType } from '@nestjs/swagger';
 import { CreateCommentDto } from './create-comment.dto';
 
 /**
- * Data transfer object for updating comment content.
+ * Data transfer object for updating comment content (re-parenting is forbidden).
  */
-export class UpdateCommentDto extends PartialType(CreateCommentDto) {}
+export class UpdateCommentDto extends OmitType(CreateCommentDto, [
+  'parentCommentId',
+] as const) {}
